a5cb5ca30a604447bf1d7d74d26b3d2f9e0eaed5
Dank3m <67323895+Dank3m@users.noreply.github.com>
2023-07-13 05:42:20 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ if (args.Length > 0)
 {
     HttpClient client = new HttpClient();
 
-    client.DefaultRequestHeaders.Add("authorization", "Bearer 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");
+    client.DefaultRequestHeaders.Add("authorization", "Bearer {api-key}");
 
     var content = new StringContent("{\"model\":\"text-davinci-001\",\"prompt\":\""+ args[0] + "\",\"temperature\":1,\"max_tokens\":100}",
         Encoding.UTF8, "application/json");
